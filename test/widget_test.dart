d8019c29b236e4a77f0d48642ccf73e7369ddc4c
Anuj Suhag <32389931+asuhag@users.noreply.github.com>
2023-07-26 19:43:11 +0100

    Change version code for release

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ScanSnapShare/main.dart';
+import 'package:ScanSnap/main.dart';
 //import 'package:image_app/main.dart';
 
 void main() {
